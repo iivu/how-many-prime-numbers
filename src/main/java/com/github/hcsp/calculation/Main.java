6060b6268fd15312232523498fb5a9941a555cc8
Bo Zhang <bo@gradle.com>
2019-06-08 17:53:45 +0800

    Init repo

---
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -0,0 +1,17 @@
+package com.github.hcsp.calculation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(howManyPrimeNumbers(100));
+    }
+
+    /**
+     * 给定一个数字n，返回1到n之间的质数（素数）个数，不包括n本身。
+     *
+     * <p>例如，n=5，返回2，因为1到5之间有2个质数：2和3。
+     *
+     * @param n 给定的数字
+     * @return 1到n之间有质数的个数
+     */
+    public static int howManyPrimeNumbers(int n) {}
+}
