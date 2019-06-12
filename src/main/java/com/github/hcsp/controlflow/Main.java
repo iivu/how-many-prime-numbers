d4fc9023175ac4e48c37ec07723fc6633aee1d46
黄楚才 <425205839@qq.com>
2019-06-12 17:36:35 +0800

    统计质数 (#4)
    统计质数 (#4)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,23 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        int result = 1;
+        label:
+        for (int i = 2; i < n; i++) {
+            for (int j = 2; j < Math.sqrt(i) + 1; j++) {
+                if (i % j == 0) {
+                    continue label;
+                }
+            }
+            result++;
+        }
+        return result;
+    }
 }
