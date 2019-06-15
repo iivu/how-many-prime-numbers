45c9d137d20de65427541a275be6f0a9a12c1d98
Bo Zhang <bo@gradle.com>
2019-06-15 19:25:11 +0800

    Update repo

--- src/test/java/com/github/hcsp/controlflow/MainTest.java
+++ src/test/java/com/github/hcsp/controlflow/MainTest.java
@@ -1,7 +1,7 @@
 package com.github.hcsp.controlflow;
 
 import java.util.Random;
-import java.util.Stream.IntStream;
+import java.util.stream.IntStream;
 import org.apache.commons.math3.primes.Primes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
