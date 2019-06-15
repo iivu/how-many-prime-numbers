953ae38cd8057387a78d336354be4ce4511c4795
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:08 +0800

    Update repo

--- src/test/java/com/github/hcsp/controlflow/MainTest.java
+++ src/test/java/com/github/hcsp/controlflow/MainTest.java
@@ -1,17 +1,16 @@
 package com.github.hcsp.controlflow;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.util.Random;
+import java.util.Stream.IntStream;
+import org.apache.commons.math3.primes.Primes;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class MainTest {
+public class MainTest {
     @Test
     public void test() {
-        int n = new java.util.Random().nextInt(1000);
-        assertEquals(
-                java.util.stream.IntStream.range(1, n)
-                        .filter(org.apache.commons.math3.primes.Primes::isPrime)
-                        .count(),
-                Main.howManyPrimeNumbers(n));
+        int n = new Random().nextInt(1000);
+        Assertions.assertEquals(
+                IntStream.range(1, n).filter(Primes::isPrime).count(), Main.howManyPrimeNumbers(n));
     }
 }
