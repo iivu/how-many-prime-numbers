b005a8bbc4c28afd9daebdb6b3974a7d6a7510fd
dsying <dingduoduo0916@gmail.com>
2019-06-26 21:37:35 +0800

    求素数 (#19)
    求素数 (#19)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.controlflow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(howManyPrimeNumbers(100));
@@ -15,5 +18,21 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        List<Integer> list = new ArrayList<>();
+        int nn = Math.abs(n);
+        for (int i = 1; i < nn ; i++) {
+            boolean isNeed = true;
+            for (int j = 2; j <= Math.sqrt(i) + 1; j++) {
+                if(i%j == 0) {
+                    isNeed = false;
+                    break;
+                }
+            }
+            if(isNeed){
+                list.add(i);
+            }
+        }
+        return list.size();
+    }
 }
