668290f70c57a0fb5bf50c42f6eb2b92ccb87a9e
JiangXi-YuYan <51253421+JiangXi-YuYan@users.noreply.github.com>
2019-06-26 13:31:19 +0800

    实现了返回给定区间内所有素数的方法。 (#18)
    实现了返回给定区间内所有素数的方法。 (#18)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.controlflow;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(howManyPrimeNumbers(100));
@@ -15,5 +17,26 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        ArrayList<Integer> isPrime = new ArrayList<>();//定义一个列表用于存放找到的质数
+        boolean flag=true;
+        int count = 0;//定义一个计数器
+        for (int i = 2; i <n ; i++) {//外层循环+最小的质数为2
+            int sqrt=(int)Math.sqrt(i);//艰涩循环次数，提高效率
+            for (int j = 2; j <(sqrt+1) ; j++) {
+                if(i%j==0){
+                    flag=false;//如果这个数不是质数，标记为false
+                    break;
+                }
+                else flag=true;//是质数标记为true
+            }
+            if (flag){
+                count++;
+                isPrime.add(i);//将所有的质数存入列表中
+            }
+        }
+            System.out.println(String.join(",", String.valueOf(isPrime)));
+            System.out.print("质数的个数为:");
+            return count;
+        }
 }
