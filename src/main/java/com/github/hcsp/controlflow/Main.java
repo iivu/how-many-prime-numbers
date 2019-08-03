3ae0fc6ef4e62fe1c24d078a95890dcef7dfcafe
Violienk <violienk@hotmail.com>
2019-08-03 22:21:54 +0800

    how-many-prime-numbers (#40)
    how-many-prime-numbers (#40)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,16 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+            int count=0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 2; j < Math.sqrt(i)+1; j++) {
+                if(0== i % j){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return n-count;
+    }
 }
