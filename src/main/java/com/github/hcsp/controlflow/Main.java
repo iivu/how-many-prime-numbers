cab104ab1e592d31e665b2d20e4d21f6eaa1b4d7
fei tang <497045233@qq.com>
2019-08-03 17:22:25 +0800

    1到N的数字中包含多少个质数 (#38)
    1到N的数字中包含多少个质数 (#38)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,24 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int result=0;
+        for(int i=1;i<n;i++){
+           if(isPrimeNumber(i)){
+               result++;
+           }
+
+        }
+        return result;
+    }
+
+    public static Boolean isPrimeNumber(int n) {
+        for(int l=2;l<Math.sqrt(n)+1;l++){
+            if(n%l==0){
+                return false;
+            }
+
+        }
+        return true;
+    }
 }
