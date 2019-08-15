1e3355224a61832e8bfe600810cc2765a5a0e064
PingAn_NetBar <840495823@qq.com>
2019-08-16 00:13:16 +0800

    判断是否是质数 (#47)
    判断是否是质数 (#47)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -3,6 +3,7 @@ package com.github.hcsp.controlflow;
 public class Main {
     public static void main(String[] args) {
         System.out.println(howManyPrimeNumbers(100));
+
     }
 
     /**
@@ -15,5 +16,25 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
-}
+    public static int howManyPrimeNumbers(int n) {
+            // 5, 2 3 4
+        int count = 0;
+        for (int i =2;i<n-1;i++){
+            if(calculate(i)){
+                count++;
+            }
+        }return  count;
+    }
+
+    private static boolean calculate(int i) {
+        int count = 0;
+        for (int j = 2; j<i;j++){
+                if(i % j != 0) {
+                    count++;
+                }
+            }if(count+2 == i) {
+            return true;
+        }return false;
+        }
+    }
+
