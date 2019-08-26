5002a50277ac8d985ee339ce0d76a6ac2758b221
kungua <52815012+kungua@users.noreply.github.com>
2019-08-26 12:36:11 +0800

    finish howManyPrimeNumbers (#54)
    finish howManyPrimeNumbers (#54)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,23 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static Boolean isPrime(int n) {
+        int end = (int) Math.floor(Math.sqrt(n));
+        for (int i = 2; i <= end; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int howManyPrimeNumbers(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
