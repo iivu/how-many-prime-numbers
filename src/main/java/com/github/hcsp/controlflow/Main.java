ee974fcd99fb09f1d48f4e50203d04e7e9054ddf
perfectLipeng <49055265+perfectLipeng@users.noreply.github.com>
2019-09-06 17:28:34 +0800

    返回给定区间质数个数 (#57)
    返回给定区间质数个数 (#57)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -7,13 +7,31 @@ public class Main {
 
     /**
      * 给定一个数字n，返回1到n之间的质数（素数）个数，不包括n本身。
-     *
+     * <p>
      * <p>例如，n=5，返回2，因为1到5之间有2个质数：2和3。
-     *
+     * <p>
      * <p>提示：对于正整数n，如果用2到Math.sqrt(n)+1之间的所有整数去除，均无法整除，则n为质数。
      *
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int primNum = 0;
+        for (int i = 2; i < n; i++) {
+
+            if (isPrimNum(i)) {
+                primNum++;
+            }
+        }
+        return primNum;
+    }
+    private static boolean isPrimNum(int n){
+        for (int j = 2; j < (int)Math.sqrt(n) + 1; j++) {
+
+            if (n % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
