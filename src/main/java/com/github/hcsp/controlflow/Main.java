23c6409d0a9dcf86ec94e02c0b68caa6322766f8
steven.wan <951520698@qq.com>
2019-09-15 19:34:31 +0800

    求质数 (#60)
    求质数 (#60)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,8 +1,12 @@
 package com.github.hcsp.controlflow;
 
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(howManyPrimeNumbers(100));
+
+        System.out.println(howManyPrimeNumbers(10));
+       System.out.println(howManyPrimeNumbers(100));
+
     }
 
     /**
@@ -15,5 +19,25 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count=0;
+        if (n>=2){
+            count+=1;
+        }
+        if (n>=3){
+            count+=1;
+        }
+        for (int j = 4; j < n; j++) {
+            for (int i = 2; i < j; i++) {
+                if (j % i == 0){
+                    break;
+                } else if (i==(j-1)) {
+                    count+=1;
+                }else {
+                    continue;
+                }
+            }
+        }
+        return count;
+    }
 }
