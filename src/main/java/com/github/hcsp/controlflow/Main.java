52c53d536d9faf043305b26d7e16b9acfd754069
JiaXinWang61 <55199085+JiaXinWang61@users.noreply.github.com>
2019-09-20 22:08:33 +0800

    求质数 (#64)
    求质数 (#64)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,16 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int m = 0;
+        for (int j=1;j<=n;j++) {
+            for (int i = 2; i < Math.sqrt(j)+1; i++) {
+                if (j % i == 0) {
+                    m++;
+                    break;
+                }
+            }
+        }
+        return n-m;
+    }
 }
