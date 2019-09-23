e01add415f35b99437c687ffa26c4aa721fa324b
Yuanmeng Xia <xymreasonxxs@163.com>
2019-09-23 21:01:00 +0800

    统计质数 (#67)
    统计质数 (#67)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -2,7 +2,7 @@ package com.github.hcsp.controlflow;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(howManyPrimeNumbers(100));
+        System.out.println(howManyPrimeNumbers(2));
     }
 
     /**
@@ -15,5 +15,20 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count = 0;
+        for (int i = 2; i < n ; i++) {
+            if (isPrime(i))
+                count++;
+        }
+        return count;
+    }
+
+    public static boolean isPrime(int num){
+        for (int i = 2; i < Math.sqrt(num) + 1; i++) {
+            if (num % i == 0 && num != 2)
+                return false;
+        }
+        return true;
+    }
 }
