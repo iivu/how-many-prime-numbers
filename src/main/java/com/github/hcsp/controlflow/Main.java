d06c6b36cf29d7f02a6253c1a5694464fd96c562
xuanzebin <38971117+xuanzebin@users.noreply.github.com>
2019-10-03 23:48:51 +0800

    feat: how many prime numbers (#71)
    feat: how many prime numbers (#71)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,16 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int result = n - 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 2; j <= Math.sqrt(i) + 1; j++) {
+                if (i % j == 0) {
+                    result--;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
