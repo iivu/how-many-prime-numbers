99d5297388079927e84da436e4f4abaaf216bf66
Llane00 <bluetinestyle@126.com>
2019-10-22 17:24:12 +0800

    add: 完善howManyPrimeNumbers函数 (#82)
    add: 完善howManyPrimeNumbers函数 (#82)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,22 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int primeNumberCount = 0;
+        for (int i = 1; i <= n; i++) {
+            if (isPrimeNumber(i)) {
+                primeNumberCount++;
+            }
+        }
+        return primeNumberCount;
+    }
+
+    public static boolean isPrimeNumber(int num) {
+        for (int i = 2; i <= (int) (Math.sqrt(num) + 1); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
