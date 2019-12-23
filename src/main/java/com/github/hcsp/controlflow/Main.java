8348c61bac62f4adeb5cd741b9518c6b6d8bc3e4
Fang Lu <huzhengen@gmail.com>
2019-12-23 17:31:58 +0800

    质数 (#113)
    质数 (#113)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,20 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < Math.sqrt(i) + 1; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
