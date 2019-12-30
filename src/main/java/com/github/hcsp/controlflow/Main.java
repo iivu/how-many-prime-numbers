4eee49e306fbd5815bb5727bdfcfe53ab6952cce
xdcgh <44773625+xdcgh@users.noreply.github.com>
2019-12-30 11:22:38 +0800

    第一版，用自己的方法去实现：求1到n之间有多少个质数（素数）。 (#117)
    第一版，用自己的方法去实现：求1到n之间有多少个质数（素数）。 (#117)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,31 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count = 0;      //累计 n 有多少个质数
+
+        for (int i = 2;     //因为 1 不是质数，所以从 2 开始
+             i <= n;
+             i++) {
+
+            boolean flag = true;    //用于判断是否为质数的标识量
+
+            //是 j 从 2 到 n 的平方根的上限值，用 temp 存储，减少每次循环时，需要计算一次开平方
+            int temp = (int) (Math.sqrt(i) + 1);
+            for (int j = 2;j < temp;j++) {
+                //判断是否为质数，如果满足条件则不是质数
+                if (i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            //如果flag还是 true 说明 i 是一个质数
+            if (flag){
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
