68206575d64765718e7775c9e58d21abeb91d938
about-Time <vipchenpost@163.com>
2020-02-06 23:04:17 +0800

    complete the function of howManyPrimeNumbers (#135)
    complete the function of howManyPrimeNumbers (#135)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,19 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int num = 0, i, j;
+        for (i = n; i >= 2; i--) {
+            for (j = 2; j <= (Math.sqrt(i) + 1); j++) {
+                if (i % j == 0 && i != j)
+                    break;
+            }
+            if (j > (Math.sqrt(i) + 1)) {
+                num++;
+            }
+        }
+        return num;
+    }
+
+
 }
