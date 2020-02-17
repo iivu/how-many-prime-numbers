8b60b3086128fe29ecdabc6dcabe5d113283cb0e
bonnie1115 <49628449+bonnie1115@users.noreply.github.com>
2020-02-17 10:05:43 +0800

    判断质数 (#144)
    判断质数 (#144)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.controlflow;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(howManyPrimeNumbers(100));
@@ -15,5 +17,26 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrimeNumber(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    private static boolean isPrimeNumber(int i) {
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
 }
