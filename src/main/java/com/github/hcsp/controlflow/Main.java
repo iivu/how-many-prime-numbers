0064667e6815f57d359d242377c24948772203c3
ARKbreeze <61083620+ARKbreeze@users.noreply.github.com>
2020-02-26 02:51:51 +0800

    完成素数计数 (#147)
    完成素数计数 (#147)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,16 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int result = n - 2;
+        for (int i = 2; i < n; i++) {
+            for (int j = 2; j <= (Math.sqrt(i) + 1); j++) {
+                if ((i % j) == 0 && i != j) {
+                    result--;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
