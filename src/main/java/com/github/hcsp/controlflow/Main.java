ff59314984cf0b25fe175f55a529769ae56f4f7f
pathjh <1639925487@qq.com>
2020-02-27 15:32:32 +0800

    统计素数 (#149)
    统计素数 (#149)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,22 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count=0;
+        if(n<2)return 0;
+        for(int i=2;i<=n;i++){
+            if(isPrimeNumber(i)) count++;
+        }
+           return count;
+    }
+    public static boolean isPrimeNumber(int num){
+        if(num==2) return true;
+        if(num<2||num%2==0)return false;
+        for(int i=3;i<=Math.sqrt(num);i+=2){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
