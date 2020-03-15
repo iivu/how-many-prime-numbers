afadd779370d1336f7d40e2a9bbc4ff25905dad1
BrightWhisper <41850676+BrightWhisper@users.noreply.github.com>
2020-03-15 19:00:14 +0800

    求1到n之间有多少个质数 (#162)
    求1到n之间有多少个质数 (#162)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,20 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count = 0;
+        if (n < 2) {
+            return 0;
+        }
+        out:
+        for (int i = 2; i < n; i++) {
+            for (int j = 2; j <= Math.sqrt(i) + 1; j++) {
+                if (i % j == 0 && j != i) {
+                    continue out;
+                }
+            }
+            count++;
+        }
+        return count;
+    }
 }
