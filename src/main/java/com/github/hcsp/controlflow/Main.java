f55f0e8b36ee1db596af689623ed13d2de69799a
ShanQing Huang <58178504+7952126@users.noreply.github.com>
2020-03-27 19:36:20 +0800

    prime (#172)
    prime (#172)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,22 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int sum = 0;
+        for (int i = 2; i < n; i++) {
+            boolean count = true;
+            for (int j = 2; j < Math.sqrt(i) + 1; j++) {
+                if (i % j == 0 && j != i) {
+                    count = false;
+                    break;
+                }
+            }
+            if (count) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
 }
+
