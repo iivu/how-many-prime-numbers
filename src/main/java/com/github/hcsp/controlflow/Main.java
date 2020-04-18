0d48facff986e43825e73849767c8d775b32f5d5
PudgeWL <nbhaohao@gmail.com>
2020-04-18 13:14:04 +0800

    feat: 完成挑战 (#178)
    feat: 完成挑战 (#178)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,19 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int num = 0;
+        primeNumberLoop:
+        for (int i = 1; i < n; i++) {
+            for (int j = 2; j <= Math.sqrt(i) + 1; j++) {
+                if (i % j == 0 && i != j) {
+                    continue primeNumberLoop;
+                }
+            }
+            if (i >= 2) {
+                num += 1;
+            }
+        }
+        return num;
+    }
 }
