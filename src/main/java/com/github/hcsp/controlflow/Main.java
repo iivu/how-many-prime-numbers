69c4c54ce2efc983f7de1cc2295773cd14a17f60
陳雲濤 <36143626+0x522@users.noreply.github.com>
2020-06-22 13:14:57 +0800

    写出区间内的质数个数。 (#202)
    写出区间内的质数个数。 (#202)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,19 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            for (int j = 2; j <= (int) Math.sqrt(i) + 1; j++) {
+                if (i == 2) {
+                    count++;
+                } else if (i % j == 0) {
+                    break;
+                } else if (j == (int) Math.sqrt(i) + 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
