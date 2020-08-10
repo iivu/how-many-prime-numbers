c9a33828f1fbe11e2db72b3bc394759f1694ef58
biantaiwen <biantaiwen@163.com>
2020-08-10 22:13:18 +0800

    fix howManyPrimeNumbers (#209)
    fix howManyPrimeNumbers (#209)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,20 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            boolean flag = true;
+            for (int j = 2; j < (Math.sqrt(i) + 1); j++) {
+                if (i % j == 0 && i != 2) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
