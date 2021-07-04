fd3a2a4e5e7da7de6d8bae8a3cf3104206e2d497
skysZhang <37018639+skysZhang@users.noreply.github.com>
2021-07-04 11:38:38 +0800

    how many prime numbers (#234)
    how many prime numbers (#234)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,22 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count = 0;
+        boolean flag = false;
+        for (int i = 2; i < n; i++) {
+            for (int j = 2; j < Math.sqrt(n) + 1; j++) {
+                if (i % j == 0 && j != i) {
+                    flag = false;
+                    break;
+                } else {
+                    flag = true;
+                }
+            }
+            if (flag) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
