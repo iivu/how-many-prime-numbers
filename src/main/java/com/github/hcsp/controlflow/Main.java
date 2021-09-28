60e21331b6f8c7d6db717b8666c26282386c7ba0
UsernameisnotavailableFeng <91192169+UsernameisnotavailableFeng@users.noreply.github.com>
2021-09-28 17:50:30 +0800

    弄了两个小时，弄完以后感觉也不难怎么用了这么长时间，还要继续努力 (#245)
    弄了两个小时，弄完以后感觉也不难怎么用了这么长时间，还要继续努力 (#245)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,17 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
-}
+    public static int howManyPrimeNumbers(int n) {
+        int b = 1;
+        for (int i = 2; i < n; i++){   //找出2~n 的整数
+            for (int a = 2; a < i; a++) {
+            if (i % a == 0) {   //判断这个整数能不能被小于它的整数整除
+                break;
+            }if (i / 2 + 1 == a) {  //如果判断到这个整数的一半加上一还没被整除说明它就是质数
+                b++;
+                }
+           }
+        }
+        return b;
+      }
+    }
