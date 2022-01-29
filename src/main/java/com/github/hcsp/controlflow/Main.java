dff1cf2359480aaaf7d4bd48237ab761794a5bff
DDB324 <65666721+DDB324@users.noreply.github.com>
2022-01-29 14:35:43 +0800

    质数 (#255)
    质数 (#255)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,17 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count = n - 1;
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 2; j < Math.sqrt(i) + 1; j++) {
+                if (i == 2) break;
+                if (i % j == 0) {
+                    count -= 1;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
 }
