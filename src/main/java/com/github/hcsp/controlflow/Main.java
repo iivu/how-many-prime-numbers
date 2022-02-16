ced0e1916fefa5d05d1b829593509f495a4cf966
赵童 <freetie@qq.com>
2022-02-17 00:01:20 +0800

    finish howManyPrimeNumbers (#257)
    finish howManyPrimeNumbers (#257)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,23 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static boolean isPrimeNumber(int n) {
+        boolean result = true;
+        for (int i = 2; i <= (Math.sqrt(n) + 1); i++) {
+            if (n % i  == 0) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+    public static int howManyPrimeNumbers(int n) {
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            if (isPrimeNumber(i)) {
+                result += 1;
+            }
+        }
+        return result;
+    }
 }
