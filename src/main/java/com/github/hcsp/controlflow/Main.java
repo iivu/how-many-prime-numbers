9b480a790ec47b8df541468a23ab15b20b1c4286
Ye Shiqing <908202572@qq.com>
2022-06-03 18:21:58 +0800

    求1到n之间有多少个质数（素数） (#268)
    求1到n之间有多少个质数（素数） (#268)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -6,6 +6,28 @@ public class Main {
     }
 
     /**
+     *
+     * @param n 任意整数
+     * @return 是否为质数
+     */
+    public static boolean isPrimeNumber(int n) {
+        if (n < 2) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        boolean flag = true;
+        for (int i = 2; i <= (int) (Math.sqrt(n)) + 1; i++) {
+            if (n % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    /**
      * 给定一个数字n，返回1到n之间的质数（素数）个数，不包括n本身。
      *
      * <p>例如，n=5，返回2，因为1到5之间有2个质数：2和3。
@@ -15,5 +37,14 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (isPrimeNumber(i)) {
+                System.out.println(i);
+                count++;
+            }
+        }
+        return count;
+    }
 }
