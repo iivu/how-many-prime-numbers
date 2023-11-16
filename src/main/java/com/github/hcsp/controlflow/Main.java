8707d6292fb09083c59a5f2a32554c23a4739040
nekoha_rf <39894049+rf818@users.noreply.github.com>
2023-11-16 09:34:25 +0800

    prime-numbers (#277)
    prime-numbers (#277)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -15,5 +15,19 @@ public class Main {
      * @param n 给定的数字
      * @return 1到n之间(不包括n)质数的个数
      */
-    public static int howManyPrimeNumbers(int n) {}
+    public static int howManyPrimeNumbers(int n) {
+        int cnt = 0;
+        for (int i = 1; i < n; i++) {
+            boolean flag = true;
+            if (i == 1) flag = false;
+            for (int j = 2; j < Math.sqrt(i) + 1; j++) {
+                if (i != j  && i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) cnt++;
+        }
+        return cnt;
+    }
 }
